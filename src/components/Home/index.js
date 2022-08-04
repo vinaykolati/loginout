fc772de99cb187a752ab90e7fd6ed62728461f66
vinay <vinaukolati4995@gmail.com>
2022-08-04 11:09:02 +0000

    login and out app

---
+++ src/components/Home/index.js
@@ -0,0 +1,36 @@
+import {Component} from 'react'
+
+import './index.css'
+
+import Message from '../Message'
+import Logout from '../Logout'
+import Login from '../Login'
+
+class Home extends Component {
+  state = {
+    isLoggedIn: false,
+  }
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="message-container">
+          <Message isLoggedIn={isLoggedIn} />
+          {isLoggedIn ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
